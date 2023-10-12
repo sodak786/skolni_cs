f575fe5097aaa4a1f832757dd17fadb892679b00
SodooSodastreamSodny <chingunsodoo786@gmail.com>
2023-10-12 09:49:11 +0200

    Přidejte soubory projektu.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Console.WriteLine("Zadej rok narození: ");
+int birthday = int.Parse(Console.ReadLine());
+
+if (birthday>=1901 & birthday<=1924){
+    Console.WriteLine("The Greatest Generation");
+}else if(birthday >= 1925 & birthday <= 1945){
+    Console.WriteLine("The Silent Generation");
+}else if (birthday >= 1946 & birthday <= 1964){
+    Console.WriteLine("The Baby Boomer Generation");
+}else if (birthday >= 1965 & birthday <= 1979){
+    Console.WriteLine("Generation X");
+}else if (birthday >= 1980 & birthday <= 1994){
+    Console.WriteLine("Millennials");
+}else if (birthday >= 1995 & birthday <= 2012){
+    Console.WriteLine("Generation Z");
+}else if (birthday >= 2013 & birthday <= 2025){
+    Console.WriteLine("Gen Alpha");
+}
+*/
---
+++ ConsoleApp1/RandomCountry.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class RandomCountry
+    {
+        public static void Main()
+        {
+            string obsah_textu = File.ReadAllText("cloud.txt");
+
+            List<string> zeme = new(obsah_textu.Split('\n'));
+            List<string>seznam_zemi = zeme.ToList();
+
+            foreach(var jedna_zeme in seznam_zemi)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("---ZEMĚ---");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(jedna_zeme);
+            }
+
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine("Chcete vygenerovat nahodnou zemi Y/N");
+            string volba =Console.ReadLine();
+            if (volba == "Y")
+            {
+                Random generator = new Random();
+                /*
+                int nahodna_pozice = generator.Next(0, seznam_zemi.Count);
+                Console.WriteLine(seznam_zemi[nahodna_pozice]);
+                */
+                int cislo_min;
+                int cislo_max;
+                Console.WriteLine("Zadej spodní hranici: ");
+                string cislo_prvni = Console.ReadLine();
+
+                Console.WriteLine("Zadej horní hranici: ");
+                string cislo_druhy = Console.ReadLine();
+
+                cislo_min = Convert.ToInt32(cislo_prvni);
+                cislo_max = Convert.ToInt32(cislo_druhy);
+
+                int nahodna_pozice = generator.Next(cislo_min, cislo_max);
+                Console.WriteLine(seznam_zemi[nahodna_pozice]);
+            }
+        }
+    }
+}
